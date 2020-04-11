e08255544eb03672b2e11e4fcf7417048c85ca84
Raymond <Raymond@yuanqingxiaodeMacBook.local>
2020-04-10 22:39:22 -0700

    proj1-0

--- proj1/a.cpp
+++ proj1/a.cpp
@@ -1,7 +1,6 @@
-
 #include <math.h>
 #include <stdlib.h>
-#include<stdio.h>
+#include <stdio.h>
 #include <time.h>
 #include <omp.h>
 
@@ -32,9 +31,8 @@ const float RMAX  =	 2.0;
 //float		Ranf( float, float );
 //int		Ranf( int, int );
 //void		TimeOfDaySeed( );
-/*helper function*/
-float
-Ranf( float low, float high )
+
+float Ranf( float low, float high )
 {
         float r = (float) rand();               // 0 - RAND_MAX
         float t = r  /  (float) RAND_MAX;       // 0. - 1.
@@ -42,8 +40,7 @@ Ranf( float low, float high )
         return   low  +  t * ( high - low );
 }
 
-int
-Ranf( int ilow, int ihigh )
+int Ranf( int ilow, int ihigh )
 {
         float low = (float)ilow;
         float high = ceil( (float)ihigh );
@@ -51,8 +48,7 @@ Ranf( int ilow, int ihigh )
         return (int) Ranf(low,high);
 }
 
-void
-TimeOfDaySeed( )
+void TimeOfDaySeed( )
 {
 	struct tm y2k = { 0 };
 	y2k.tm_hour = 0;   y2k.tm_min = 0; y2k.tm_sec = 0;
@@ -64,44 +60,44 @@ TimeOfDaySeed( )
 	unsigned int seed = (unsigned int)( 1000.*seconds );    // milliseconds
 	srand( seed );
 }
+
 // main program:
-int
-main( int argc, char *argv[ ] )
+int main( int argc, char *argv[ ] )
 {
 #ifndef _OPENMP
 	fprintf( stderr, "No OpenMP support!\n" );
 	return 1;
 #endif
 
-	float tn = tan( (M_PI/180.)*30. );
 	TimeOfDaySeed( );		// seed the random number generator
 
 	omp_set_num_threads( NUMT );	// set the number of threads to use in the for-loop:`
-	
+
 	// better to define these here so that the rand() calls don't get into the thread timing:
 	float *xcs = new float [NUMTRIALS];
 	float *ycs = new float [NUMTRIALS];
 	float * rs = new float [NUMTRIALS];
 
-        // fill the random-value arrays:
-        for( int n = 0; n < NUMTRIALS; n++ )
-        {       
-                xcs[n] = Ranf( XCMIN, XCMAX );
-                ycs[n] = Ranf( YCMIN, YCMAX );
-                rs[n] = Ranf(  RMIN,  RMAX ); 
-        }       
+    // fill the random-value arrays:
+    for( int n = 0; n < NUMTRIALS; n++ )
+    {
+        xcs[n] = Ranf( XCMIN, XCMAX );
+        ycs[n] = Ranf( YCMIN, YCMAX );
+        rs[n] = Ranf(  RMIN,  RMAX );
+    }
 
-        // get ready to record the maximum performance and the probability:
-        float maxPerformance = 0.;      // must be declared outside the NUMTRIES loop
-        float currentProb;              // must be declared outside the NUMTRIES loop
+    // get ready to record the maximum performance and the probability:
+    float maxPerformance = 0.;      // must be declared outside the NUMTRIES loop
+    float currentProb;              // must be declared outside the NUMTRIES loop
 
-        // looking for the maximum performance:
-        for( int t = 0; t < NUMTRIES; t++ )
-        {
-                double time0 = omp_get_wtime( );
+    // looking for the maximum performance:
+    for( int t = 0; t < NUMTRIES; t++ )
+    {
+        double time0 = omp_get_wtime( );
 
-                int numHits = 0;
-		#pragma omp parallel for default(none) shared(xcs,ycs,rs,tn) reduction(+:numHits)
+        int numHits = 0;
+
+		#pragma omp parallel for default(none) shared(xcs,ycs,rs) reduction(+:numHits)
 		for( int n = 0; n < NUMTRIALS; n++ )
 		{
 			// randomize the location and radius of the circle:
@@ -110,34 +106,34 @@ main( int argc, char *argv[ ] )
 			float  r =  rs[n];
 
 			// solve for the intersection using the quadratic formula:
-			float a = 1. + tn*tn;
-			float b = -2.*( xc + yc*tn );
+			float a = 2.;
+			float b = -2.*( xc + yc );
 			float c = xc*xc + yc*yc - r*r;
 			float d = b*b - 4.*a*c;
-// If d is less than 0., then the circle was completely missed. (Case A) Continue on to the next trial in the for-loop.
 
-			if(d<0)
-				continue;
+			if( d < 0. )
+			    continue;
+
 			// hits the circle:
 			// get the first intersection:
 			d = sqrt( d );
 			float t1 = (-b + d ) / ( 2.*a );	// time to intersect the circle
 			float t2 = (-b - d ) / ( 2.*a );	// time to intersect the circle
 			float tmin = t1 < t2 ? t1 : t2;		// only care about the first intersection
-// If tmin is less than 0., then the circle completely engulfs the laser pointer. (Case B) Continue on to the next trial in the for-loop.
-			if(tmin<0)
-				continue;
+
+			if( tmin < 0. )
+		        continue;
 
 			// where does it intersect the circle?
 			float xcir = tmin;
-			float ycir = tmin*tn;
+			float ycir = tmin;
 
 			// get the unitized normal vector at the point of intersection:
 			float nx = xcir - xc;
 			float ny = ycir - yc;
-			float nxy = sqrt( nx*nx + ny*ny );
-			nx /= nxy;	// unit vector
-			ny /= nxy;	// unit vector
+			float n = sqrt( nx*nx + ny*ny );
+			nx /= n;	// unit vector
+			ny /= n;	// unit vector
 
 			// get the unitized incoming vector:
 			float inx = xcir - 0.;
@@ -152,14 +148,12 @@ main( int argc, char *argv[ ] )
 			float outy = iny - 2.*ny*dot;	// angle of reflection = angle of incidence`
 
 			// find out if it hits the infinite plate:
-			float tt = ( 0. - ycir ) / outy;
-// If tt is less than 0., then the reflected beam went up instead of down. Continue on to the next trial in the for-loop.
+			float t = ( 0. - ycir ) / outy;
 
-//Otherwise, this beam hit the infinite plate. (Case D) Increment the number of hits and continue on to the next trial in the for-loop.
-			if (tt<0) 
-				continue;
-			else 
-				numHits++;
+			if( t < 0. )
+			    continue;
+			else
+			    numHits += 1;
 
 		}
 		double time1 = omp_get_wtime( );
