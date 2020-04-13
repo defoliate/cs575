de5ce816e34adb079aeaa5fee2cc954e4449b1ad
Qingxiao Yuan <yuanqi@flip3.engr.oregonstate.edu>
2020-04-12 23:19:38 -0700

    fix python3 bug

--- proj1/proj1.py
+++ proj1/proj1.py
@@ -1,9 +1,9 @@
 import os
-for t in [ 1, 2, 4, 8 ]:
-    print "NUMT = %d" % t
-    for s in [100,10000,100000,1000000]:
-	print "NUMTRIALS = %d" % s
-	cmd = "g++ -DNUMTRIALS=%d -DNUMT=%d prog.cpp -o prog -lm -fopenmp" % ( s, t )
-	os.system( cmd )
-	cmd = "./prog"
-	os.system( cmd )
+for t in  1, 2, 4, 8 :
+    print( "NUMT = %d" % t)
+    for s in 100,10000,100000,1000000:
+        print("NUMTRIALS = %d" % s)
+        cmd = "g++ -DNUMTRIALS=%d -DNUMT=%d prog.cpp -o prog -lm -fopenmp" % ( s, t )
+        os.system( cmd )
+        cmd = "./prog"
+        os.system( cmd )
