9705dcf6e0d4b16baecdf53e89ecd46602a40781
Raymond <Raymond@yuanqingxiaodeMacBook.local>
2020-04-14 15:56:27 -0700

    p0-test

--- proj0/p0.py
+++ proj0/p0.py
@@ -1,6 +1,6 @@
 import os
 for t in [ 1, 4 ]:
-	print "NUMT = %d" % t
+	print ("NUMT = %d" % t)
 	for s in [ 2, 4,8,16,32,64, 128, 256,512]:
 		#print "NUMS = %d" % s
 		cmd = "g++ -DNUMS=%d -DNUMT=%d p0.cpp -o p0 -lm -fopenmp" % ( s, t )
