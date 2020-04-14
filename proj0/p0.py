c7c76b76ae458960707e24dc1a221cf6e39a5a79
Raymond <Raymond@yuanqingxiaodeMacBook.local>
2020-04-14 15:23:45 -0700

    p0 archieve-0

--- proj0/p0.py
+++ proj0/p0.py
@@ -3,7 +3,7 @@ for t in [ 1, 4 ]:
 	print "NUMT = %d" % t
 	for s in [ 2, 4,8,16,32,64, 128, 256,512]:
 		#print "NUMS = %d" % s
-		cmd = "g++ -DNUMS=%d -DNUMT=%d a.cpp -o prog -lm -fopenmp" % ( s, t )
+		cmd = "g++ -DNUMS=%d -DNUMT=%d p0.cpp -o p0 -lm -fopenmp" % ( s, t )
 		os.system( cmd )
-		cmd = "./prog"
+		cmd = "./p0"
 		os.system( cmd )
