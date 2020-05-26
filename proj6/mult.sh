2ed76b1194d213eb5d2582121efd6c476e24bb0a
Raymond <Raymond@yuanqingxiaodeMacBook.local>
2020-05-26 02:19:38 -0700

    dizzy in first.cpp

--- proj6/mult.sh
+++ proj6/mult.sh
@@ -17,21 +17,22 @@
 # 	for s in  1024 32768 131072 262144 524288 1048576 4194304 8388608
 # 	do
 # 		echo GLOBAL_SIZE = $s
-# 		g++ -DNMB=$n -DLOCAL_SIZE=$t -o proj first.cpp /usr/local/apps/cuda/cuda-10.1/lib64/libOpenCL.so.1.1 -lm -fopenmp
+# 		g++ -DLOCAL_SIZE=$t -DGLOBAL_SIZE=$s multiply.cpp -o prog /scratch/cuda-7.0/lib64/libOpenCL.so -lm -fopenmp
 # 		./prog
 # 	done
 # done
-# rm prog
+
 #!/bin/bash
-# number of threads:
-for t in 8 32 64 128 256 512
+
+# number of threads
+for gs in 1024 32768 131072 262144 524288 1048576 4194304 8388608
 do
-	echo LOCAL_SIZE = $t
-	# number of subdivisions:
-	for s in  1024 32768 131072 262144 524288 1048576 4194304 8388608
-	do
-		echo GLOBAL_SIZE = $s
-		g++ -DLOCAL_SIZE=$t -DGLOBAL_SIZE=$s multiply.cpp -o prog /scratch/cuda-7.0/lib64/libOpenCL.so -lm -fopenmp
-		./prog
-	done
+    # number of subdivisions:
+    for ls in 32 64 128 256
+    do
+	let ng=$gs/$ls
+        g++ -DNUM_ELEMENTS=$gs -DLOCAL_SIZE=$ls -DNUM_WORK_GROUPS=$ng -o mult mult.cpp /usr/local/apps/cuda/cuda-10.1/lib64/libOpenCL.so.1.1 -lm -fopenmp
+        ./mult
+    done
 done
+rm mult
