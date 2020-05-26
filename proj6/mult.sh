47cf03050a2fd3b2a59e1d353e1c7aeccd1755a3
Raymond <Raymond@yuanqingxiaodeMacBook.local>
2020-05-26 02:04:07 -0700

    change mult

--- proj6/mult.sh
+++ proj6/mult.sh
@@ -10,6 +10,20 @@
 
 #!/bin/bash
 # number of threads:
+# for t in 8 32 64 128 256 512
+# do
+# 	echo LOCAL_SIZE = $t
+# 	# number of subdivisions:
+# 	for s in  1024 32768 131072 262144 524288 1048576 4194304 8388608
+# 	do
+# 		echo GLOBAL_SIZE = $s
+# 		g++ -DNMB=$n -DLOCAL_SIZE=$t -o proj first.cpp /usr/local/apps/cuda/cuda-10.1/lib64/libOpenCL.so.1.1 -lm -fopenmp
+# 		./prog
+# 	done
+# done
+# rm prog
+#!/bin/bash
+# number of threads:
 for t in 8 32 64 128 256 512
 do
 	echo LOCAL_SIZE = $t
@@ -17,8 +31,7 @@ do
 	for s in  1024 32768 131072 262144 524288 1048576 4194304 8388608
 	do
 		echo GLOBAL_SIZE = $s
-		g++ -DNMB=$n -DLOCAL_SIZE=$t -o proj first.cpp /usr/local/apps/cuda/cuda-10.1/lib64/libOpenCL.so.1.1 -lm -fopenmp
+		g++ -DLOCAL_SIZE=$t -DGLOBAL_SIZE=$s multiply.cpp -o prog /scratch/cuda-7.0/lib64/libOpenCL.so -lm -fopenmp
 		./prog
 	done
 done
-rm prog
