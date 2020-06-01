330c4d00c43d0dd655cd80fe185f3750900d249b
Qingxiao Yuan <yuanqi@flip3.engr.oregonstate.edu>
2020-06-01 02:31:03 -0700

    proj6 turn in

--- proj6/mult.sh
+++ proj6/mult.sh
@@ -4,7 +4,7 @@
 for gs in 1024 32768 131072 262144 524288 1048576 4194304 8388608
 do
     # number of subdivisions:
-    for ls in 32 64 128 256
+    for ls in 8 32 64 128 256 512
     do
 	let ng=$gs/$ls
         g++ -DNUM_ELEMENTS=$gs -DLOCAL_SIZE=$ls -DNUM_WORK_GROUPS=$ng -o mult mult.cpp /usr/local/apps/cuda/cuda-10.1/lib64/libOpenCL.so.1.1 -lm -fopenmp
---
+++ proj6/submit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#BATCH -J MatrixMult 
+#SBATCH -A cs475-575
+#SBATCH -p class
+#SBATCH --gres=gpu:1
+#SBATCH -o p6.out
+#SBATCH -e p6.err
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=yuanqi@oregonstate.edu
+./mult.sh
+
